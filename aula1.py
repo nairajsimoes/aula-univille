c77f336515eba3ec20fd454b401d018b16fbc34b
Nai <nairasimoes44@gmail.com>
2026-03-28 11:15:41 -0300

    .

---
+++ aula1.py
@@ -0,0 +1,9 @@
+nome = input("Digite seu nome: ")
+print(f"Olá, {nome}! Bem-vindo(a) à aula de Python!")
+
+numero = int(input('Digite um número para ver quanto é o dobro: '))
+print(f'O dobro de {numero} é {numero * 2}!')
+
+soma = int(input(f'Digite um número: ')) 
+soma2 = int(input('Digite outro número: '))
+print(f'A soma de {soma} + {soma2} é {soma + soma2}!')
