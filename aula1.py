5eae94bd6ec41c46e8e17477790a7f7b65dc3362
Nai <nairasimoes44@gmail.com>
2026-03-28 19:54:54 -0300

    Update aula1.py

--- aula1.py
+++ aula1.py
@@ -1,3 +1,5 @@
+from datetime import date 
+
 nome = input("Digite seu nome: ")
 print(f"Olá, {nome}! Bem-vindo(a) à aula de Python!")
 
@@ -7,3 +9,8 @@ print(f'O dobro de {numero} é {numero * 2}!')
 soma = int(input(f'Digite um número: ')) 
 soma2 = int(input('Digite outro número: '))
 print(f'A soma de {soma} + {soma2} é {soma + soma2}!')
+
+ano_atual = date.today().year
+ano_nascimento = int(input('Digite seu ano de nascimento: '))
+idade = ano_atual - ano_nascimento
+print(f'Você tem {idade} anos!')
